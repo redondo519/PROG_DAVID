96af421aa4af46db871d35d420a21f24ddebc4c8
redondo519 <david.redalo@iesjulianmarias.es>
2024-10-18 18:49:16 +0200

    david

---
+++ condicionales/src/ejcondi1.java
@@ -0,0 +1,8 @@
+public class ejcondi1 {
+    public static void main(String[] args) {
+        System.out.println("DAVID");
+        System.out.println("DAVID");
+        System.out.println("DAVID");
+
+    }
+}
