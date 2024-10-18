e91ff4a6e4d434d52c98654a011da9fa4129445c
redondo519 <david.redalo@iesjulianmarias.es>
2024-10-18 18:38:40 +0200

    ejercicios condicionales david

--- EJERCICIOS_CONDICIONALES/src/E1cond.java
+++ EJERCICIOS_CONDICIONALES/src/E1cond.java
@@ -2,6 +2,7 @@ public class E1cond {
     public static void main(String[] args) {
     System.out.println("Hola");
         System.out.println("Hola");
+        System.out.println("Hola");
 
 
     }
