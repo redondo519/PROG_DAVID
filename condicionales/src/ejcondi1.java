c945ff5e25ad7f4a46af18002063ae9fd4818242
redondo519 <david.redalo@iesjulianmarias.es>
2024-10-18 19:12:18 +0200

    david2

--- condicionales/src/ejcondi1.java
+++ condicionales/src/ejcondi1.java
@@ -1,9 +1,27 @@
+
+//Es necesario importar la utilidad de escaner para leer datos
+import javax.swing.*;
+import java.util.Scanner;
+
 public class ejcondi1 {
     public static void main(String[] args) {
-        System.out.println("DAVID");
-        System.out.println("DAVID");
-        System.out.println("DAVID");
-        System.out.println("DAVID");
+
+        //Declariacion de variable ENTERA para numero especificado
+        int num ;
+
+        //Solicitamos numero
+        System.out.println("Introduce un número para comprobar su paridad  ");
+
+        //Leemos numero y lo guardamos en num(int)
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+
+        //Comprobamos paridad
+        if (num % 2 == 0) {
+            System.out.printf("El numero %d es par ",num);
+        }else{
+            System.out.printf("El numero %d es impar: ",num);
+        }
 
 
     }
