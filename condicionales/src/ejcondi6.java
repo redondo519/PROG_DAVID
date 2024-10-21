703314557dbb6c4978dfff40ca234271dcc33fed
redondo519 <david.redalo@iesjulianmarias.es>
2024-10-21 19:35:05 +0200

    ejercicios 5

--- condicionales/src/ejcondi6.java
+++ condicionales/src/ejcondi6.java
@@ -4,39 +4,51 @@ public class ejcondi6 {
     public static void main(String[] args) {
 
         //Declariacion de variables enteras
-        int num1 ;
-        int num2 ;
-        int num3 ;
+        int a ;
+        int b ;
+        int c ;
 
         //Solicitamos numeros
-        System.out.println("Introduce tres numeros: ");
+        System.out.println("Introduce tres numeros enteros: ");
 
         //Almacenamos numeros
         Scanner sc = new Scanner(System.in);
-        num1 = sc.nextInt();
-        num2 = sc.nextInt();
-        num3 = sc.nextInt();
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
 
-        //Condicional: Comprobamos cual es mayor para ordenar los numeros
-
-        if (num1 > num2 && num1 > num3 && num2 > num3) {
-
-            System.out.printf(" %d  %d %d", num1, num2, num3);
-
-        }else{
-            System.out.printf(" %d  %d %d", num2, num3, num1);
+        //Condicional: Comprobamos cual es mayor para ser el primero en mostar
+        if (a > b && a > c){
+            //Si el primero es a, comrobar cual es el mayor entre b y c para = SEGUNDO
+            if (b > c){
+                System.out.println("Orden mayor a menor: " + a + "," + b + "," + c);
+            }else{
+                System.out.println("Orden mayor a menor: " + a + "," + c + "," + b);
+            }
         }
 
+        //Comprobamos, si b es el mayor
+        if (b > a && b > c){
+            if (a > c){
+                System.out.println("Orden mayor a menor: " + b + "," + a + "," + c);
+            }else{
+                System.out.println("Orden mayor a menor: " + b + "," + c + "," + a);
+            }
         }
 
+        //Comprobamos, si c es el mayor
+        if (c > a && c > b){
+            if (a > b){
+                System.out.println("Orden mayor a menor: " + c + "," + a + "," + b);
+            }else{
+                System.out.println("Orden mayor a menor: " + c + "," + b + "," + a);
+            }
+        }
 
-
-
-
-
-
-
-
+        if (a == b && a == c){
+            System.out.println("Los numeros " + a + "," + b + "," + c + " son iguales");
+        }
 
     }
 
+}
