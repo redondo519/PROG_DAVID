36a2df367a993ca1f46c262c80bd14359a27f8dd
redondo519 <david.redalo@iesjulianmarias.es>
2024-10-23 20:25:08 +0200

    e

--- ejercicios_bucles_entrega_DRA/src/ejbucle3.java
+++ ejercicios_bucles_entrega_DRA/src/ejbucle3.java
@@ -5,11 +5,23 @@ public class ejbucle3 {
 
         //Variables
         Scanner sc = new Scanner(System.in);
-        int n,suma,total,mayores;
+        int n,total,mayores;
+        int suma=0;
 
 
         System.out.println("Ingrese la edad del alumno:");
         n = sc.nextInt();
+        suma = suma + n;
+        System.out.println("La suma de los alumnos es: "+suma);
+        System.out.println("La edad media de los alumnos es: "+suma/n);
+
+        if (n!=0) {
+            if (n > 18) {
+                //leer enunciado
+            }
+        }else{
+            System.out.println("ERROR");
+        }
 
 
 
