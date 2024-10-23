8bee08aaa72f60b50859e5a847d89aa34bb2b773
redondo519 <david.redalo@iesjulianmarias.es>
2024-10-23 19:04:58 +0200

    ejercicio bucle 1

---
+++ ejercicios_bucles_entrega_DRA/src/ejbucle1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ejbucle1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el primer numero: ");
+
+    }
+}
