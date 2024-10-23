a7f70bcfa7403e695c7166d139e0fecb29f54179
redondo519 <david.redalo@iesjulianmarias.es>
2024-10-23 19:39:01 +0200

    ejercicio bucle 1

--- ejercicios_bucles_entrega_DRA/src/ejbucle1.java
+++ ejercicios_bucles_entrega_DRA/src/ejbucle1.java
@@ -2,8 +2,34 @@ import java.util.Scanner;
 
 public class ejbucle1 {
     public static void main(String[] args) {
+
+        //Variables
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese el primer numero: ");
+        int edad = 0;
+        int max = 0;
+        int min = 200;
+
+        
+
+
+        //bucle pedir edad mientras usuario no ingrese -1
+        do {
+
+            //Solicitud de edad
+            System.out.println("Ingrese la edad del primer alumno o Ingrese   -1  para salir: ");
+            edad = sc.nextInt();
+
+            //Calculo edad máxima
+            if (edad > max) {
+                max = edad;
+            }else if (edad < min && edad >-1) {
+                min = edad;
+            }
+
+        }while (edad != -1); //Cierre de while
+        System.out.println("La edad maxima de los alumnos es : " + max);
+        System.out.println("La edad minima de los alumnos es : " + min);
+
 
     }
 }
