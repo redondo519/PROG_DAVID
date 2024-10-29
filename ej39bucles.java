4bfced3d8890eb71aaea7828c4a735d10ff0793e
https://github.com/redondo519/PROG_DAVID.git <david.redalo@iesjulianmarias.es>
2024-10-30 00:58:35 +0100

    comit

--- ej39bucles.java
+++ ej39bucles.java
@@ -1,2 +1,11 @@
 public class ej39bucles {
+    public static void main(String[] args) {
+
+        //pide un numero, muestra impares desde 1 hasta n
+        //declaracion de variables
+        int n;
+        
+        //Solicitud de n
+
+    }
 }
