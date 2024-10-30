70f39d2e09faf39170d42f68a2cb77141c0317da
https://github.com/redondo519/PROG_DAVID.git <david.redalo@iesjulianmarias.es>
2024-10-30 01:28:42 +0100

    EJ40 HORAS

--- bucles_3/src/ej40bucles.java
+++ bucles_3/src/ej40bucles.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class ej40bucles {
     public static void main(String[] args) {
 
+        //Saluda en funcion de la hora introducida
+
+        //Declaracion de variables
+        Scanner sc = new Scanner(System.in);
+        int h;
+
+        //Solicitud de hora completa
+        System.out.println("Ingrese el valor de la hora:");
+        h = sc.nextInt();
+
+
+
+        if ((h<0) || (h>24)) {
+            System.out.println("HORA MAL INTRODUCIDA");
+        }else {
+
+
+            //Rangos para el saludo
+            if (h >= 6 && h <= 12) {
+
+                System.out.println("BUENOS DÍAS");
+                System.out.println("Son las " + h);
+
+            } else if (h >= 13 && h <= 20) {
+
+                System.out.println("BUENAS TARDES");
+                System.out.println("Son las " + h);
+
+            } else if (h >= 21 && h <= 24 || (h >= 0 && h <= 5)) {
+                System.out.println("BUENAS NOCHES");
+                System.out.println("Son las " + h);
+            }
+
+        }
     }
 }
+
