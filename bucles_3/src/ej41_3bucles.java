a33fed8dff9f768ffcf8d61efa8818417d00fec1
https://github.com/redondo519/PROG_DAVID.git <david.redalo@iesjulianmarias.es>
2024-10-30 01:41:12 +0100

    comit

--- bucles_3/src/ej41_3bucles.java
+++ bucles_3/src/ej41_3bucles.java
@@ -1,18 +1,18 @@
-public class ej41_2 {
+public class ej41_3bucles {
     public static void main(String[] args) {
-        //BUCLE WHILE
+        //BUCLE DO
 
         //Declaracion de variable para contador
         int i = 5;
 
-        //Bucle while para hayar todos los numeros entre 5 y 100
-        while(i<=100) {
+        //Bucle do para hayar todos los numeros entre 5 y 100
+        do{
         //Condicion para ser multiplo de 5
             if (i % 5 == 0){
                 System.out.println(i);
             }
             i++;
 
-        }
+        }while(i<=100) ;
     }
 }
