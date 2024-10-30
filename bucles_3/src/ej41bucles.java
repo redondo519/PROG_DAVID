7060432cc0275895a39abd7e7ee115797043e6a8
redondo519 <david.redalo@iesjulianmarias.es>
2024-10-30 17:25:38 +0100

    comit

--- bucles_3/src/ej41bucles.java
+++ bucles_3/src/ej41bucles.java
@@ -6,11 +6,11 @@ public class ej41bucles {
         int i;
 
         //bucle desde el primer multiplo de 5 (5) hasta 100
-        for (i = 5; i <= 100; i++) {
+        for (i = 5; i <= 100; i+=5) {
             //Muestra mediante condicion solo los numeros multiplos de 5
-            if (i % 5 == 0) {
+
                 System.out.println(i);
-            }
+
 
         }
     }
