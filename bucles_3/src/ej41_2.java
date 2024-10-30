889f673d6e6cab23f2f8812c6a71d558d4581710
https://github.com/redondo519/PROG_DAVID.git <david.redalo@iesjulianmarias.es>
2024-10-30 01:38:50 +0100

    EJ41 while,multiploo5

--- bucles_3/src/ej41_2.java
+++ bucles_3/src/ej41_2.java
@@ -1,5 +1,18 @@
 public class ej41_2 {
     public static void main(String[] args) {
         //BUCLE WHILE
+
+        //Declaracion de variable para contador
+        int i = 5;
+
+        //Bucle while para hayar todos los numeros entre 5 y 100
+        while(i<=100) {
+
+            if (i % 5 == 0){
+                System.out.println(i);
+            }
+            i++;
+
+        }
     }
 }
