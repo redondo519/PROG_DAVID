c181100f49e38b5cc9c3ed1502b29c997432566c
https://github.com/redondo519/PROG_DAVID.git <david.redalo@iesjulianmarias.es>
2024-11-02 20:40:22 +0100

    ej 52 calculos con pares e impares

---
+++ bucles_3/src/ej52buclesCalculos.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class ej52buclesCalculos {
+    public static void main(String[] args) {
+
+        //declaracion de variables
+        Scanner sc = new Scanner(System.in);
+        int num; //Numero para introducir
+        int i = 0; //variable contador total numeros introducidos
+        double j = 0; //variable contador numeros impares para media
+        int mayor = 0; //almacena el num par mayor
+        double suma = 0;
+        double media = 0; //para calculo media de los impares
+
+        //bucle pedir nums distintos hasta que num -> negativo
+        do{
+            //solicitamos numero
+            System.out.println("Ingrese un numero: ");
+            num = sc.nextInt();
+
+            //aumentamos contador
+
+
+
+            //Excluir a los negativos
+            if (num >= 0){
+                i++;
+
+            }
+
+            //Comprobamos paridad
+            if (num % 2 == 0){ //pares
+
+                if (num > mayor) {
+                    mayor = num; //almacenamos mayor
+                }
+
+            }else{ //impares
+
+                if (num >= 0) { //excluimos numeros negativos
+                    j++;
+                    suma = suma + num; //realizamos suma
+                    media = suma/j; //almacenamos media
+                }
+
+
+            }
+
+        }while(num >=0);
+
+        System.out.printf("Se han introducido un total de %d numeros positivos: \n ",i);
+        System.out.printf("Se han introducido un total de %f numeros IMPARES: \n ",j);
+
+        System.out.println("suma impares: "+suma);
+        System.out.println("La media de los numeros impares es: " + media);
+        System.out.println("El mayor introducido  de los numeros pares es: " + mayor);
+
+    }
+}
