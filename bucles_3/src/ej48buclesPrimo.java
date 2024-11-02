7b52dd754be06581a5f699b2509d4032dfd496d8
https://github.com/redondo519/PROG_DAVID.git <david.redalo@iesjulianmarias.es>
2024-11-02 19:06:00 +0100

    48 primos con boolean y comprobacion de positivos

--- bucles_3/src/ej48buclesPrimo.java
+++ bucles_3/src/ej48buclesPrimo.java
@@ -10,26 +10,25 @@ public class ej48buclesPrimo {
         Scanner sc = new Scanner(System.in);
 
 
-        //Solicitamos numero num(int)
-        System.out.println("Ingrese un numero");
-        num = sc.nextInt();
+        //Bucle comprobacion num es positivo
+        do{
+            //Solicitamos numero num(int)
+            System.out.println("Ingrese un numero");
+            num = sc.nextInt();
+        }while(num <=0);
 
+
+        //comprobamos todas las posibilidades de divisores entre 2 y n-1; Si hay un divisor o más es primo
         for (i = 2; i < num; i++) {
             if (num % i == 0) {
                 primo = false;
             }
         }
-        if (primo) {
+        if (primo) { //Para primo false: no se encontraron más divisores
             System.out.printf("El numero %d es primo",num);
-        }else{
+        }else{ //Para primo true: hay + de un divisor entre 2 y n-1
             System.out.printf("El numero %d no es primo",num);
         }
-
-
-
-       
-
-
 
     }
 }
