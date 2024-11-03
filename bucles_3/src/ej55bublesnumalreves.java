688e8a713974a25a058e4960ae210da25b0e4957
https://github.com/redondo519/PROG_DAVID.git <david.redalo@iesjulianmarias.es>
2024-11-03 15:49:14 +0100

    comit

---
+++ bucles_3/src/ej55bublesnumalreves.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ej55bublesnumalreves {
+    public static void main(String[] args) {
+        //declaracion de variables
+        Scanner sc = new Scanner(System.in);
+        int num;
+        int numReves;
+
+        //Solicitud de num
+        System.out.print("Ingrese un numero: ");
+        num = sc.nextInt();
+
+
+
+    }
+}
