208ef58264c506774e66d9092350a50eb7f1b7af
redondo519 <david.redalo@iesjulianmarias.es>
2024-11-08 17:51:51 +0100

    comit excepciones

--- excepciones/src/ejexcepciones61.java
+++ excepciones/src/ejexcepciones61.java
@@ -1,4 +1,17 @@
-public class ejexcepciones61 {
-    //Declaracion de variables
+import java.util.Scanner;
+
+public class ejexcepciones61  {
+    public static void main(String[] args) {
+        //Declaracion variables
+        Scanner entrada = new Scanner(System.in);
+        String a;
+        int num;
+
+        //Solicitamos cadena de caracteres
+        System.out.println("Introduce una cadena de caracteres");
+        a = entrada.nextLine();
+        num = Integer.parseInt(a);
+
+    }
 
 }
