1edb9142f80c80d1720aa051cb5500538fc21e67
redondo519 <david.redalo@iesjulianmarias.es>
2024-11-11 20:39:18 +0100

    comit ejemplo 1

---
+++ arrays/src/arrays1_1.java
@@ -0,0 +1,10 @@
+public class arrays1_1 {
+    public static void main(String[] args) {
+
+        int num[] = {22,24};
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(num[i]);
+        }
+
+    }
+}
