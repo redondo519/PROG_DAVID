a0cf7a4099d6318f90503ce5c126a7c24750bea0
redondo519 <david.redalo@iesjulianmarias.es>
2024-11-11 20:43:10 +0100

    comit ejemplo 1

---
+++ arrays/src/ejarrays1.java
@@ -0,0 +1,16 @@
+public class ejarrays1 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+
+
+
+        }
+        int num[] = {23,24};
+
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(num[i]);
+        }
+
+    }
+}
