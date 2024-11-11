e2b1ba4c9c7151afb88b7becb802b592a5321e87
redondo519 <david.redalo@iesjulianmarias.es>
2024-11-11 21:03:30 +0100

    comit

---
+++ ARRAYSS/src/ejearrays1.java
@@ -0,0 +1,15 @@
+public class ejearrays1 {
+    public static void main(String[] args) {
+        int numero[] = new int [10];
+
+        for (int i = 0; i < numero.length; i++) {
+
+            numero[i] = (int) (Math.random()*100);
+
+        }
+
+        for (int i = 0; i < numero.length; i++) {
+            System.out.print(numero[i] + " | ");
+        }
+    }
+}
